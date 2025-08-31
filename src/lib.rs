ae241abb03bec36f94f2289820f0f94d1e783c28
luboise <lucas2@live.com.au>
2025-09-01 09:58:50 +1000

    Added model extraction

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ pub mod asset;
 use byteorder::{LittleEndian, ReadBytesExt};
 
 use std::{
+    cmp,
     error::Error,
     fmt::Display,
     io::{Cursor, Read, Seek, SeekFrom},
@@ -478,7 +479,7 @@ impl Display for VirtualResourceError {
 }
 
 impl VirtualResource<'_> {
-    pub fn from_dvl<'a>(
+    pub(crate) fn from_dvl<'a>(
         dataview_list: &DataViewList,
         bytes: &'a [u8],
     ) -> Result<VirtualResource<'a>, VirtualResourceError> {
@@ -504,34 +505,61 @@ impl VirtualResource<'_> {
 
     pub fn get_bytes(
         &self,
-        offset_n: usize,
-        size_n: usize,
+        start_offset: usize,
+        get_size: usize,
     ) -> Result<Vec<u8>, VirtualResourceError>
 where {
-        let offset = offset_n.into();
-        let size = size_n.into();
-
         let end = self.len();
 
-        if end < offset {
+        if end < start_offset {
             return Err(VirtualResourceError::OffsetOutOfBounds);
-        } else if end - offset < size {
+        } else if end - start_offset < get_size {
             return Err(VirtualResourceError::SizeOutOfBounds);
         }
 
-        /*
-        views.iter().fold(0usize, |offset, view| {
-            let view_offset = view.offset as usize;
-            let copy_size = view.size as usize;
+        let mut v = vec![0; get_size];
 
-            bytes[offset..offset + copy_size]
-                .copy_from_slice(&data[view_offset..view_offset + copy_size]);
+        let mut slice_start = 0usize;
+        let mut total_written = 0usize;
 
-            offset + new_offset as usize
-        });
-        */
+        for slice in &self.slices {
+            let slice_size = slice.len();
 
-        todo!();
+            // If this slice is part of the copy in any way
+            if (slice_start + slice_size) > start_offset {
+                let desired_cp_size = get_size - total_written;
+
+                // Get start index
+                let cp_i = start_offset.saturating_sub(slice_start);
+                let cp_size = cmp::min(desired_cp_size, slice_size - cp_i);
+
+                let cp_j = cp_i + cp_size;
+
+                v[total_written..total_written + cp_size].copy_from_slice(&slice[cp_i..cp_j]);
+
+                total_written += cp_size;
+
+                if total_written > get_size {
+                    return Err(VirtualResourceError::SizeOutOfBounds);
+                } else if total_written == get_size {
+                    break;
+                }
+            }
+
+            slice_start += slice_size;
+        }
+
+        if total_written != get_size {
+            return Err(VirtualResourceError::SizeOutOfBounds);
+        }
+
+        Ok(v)
+    }
+
+    pub(crate) fn from_slices<'a>(slices: &'a [&[u8]]) -> VirtualResource<'a> {
+        VirtualResource {
+            slices: slices.to_vec(),
+        }
     }
 
     pub fn len(&self) -> usize {
@@ -542,5 +570,41 @@ where {
 
     pub fn is_empty(&self) -> bool {
         self.len() == 0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const fn make_data<const N: usize>() -> [u8; N] {
+        let mut arr = [0u8; N];
+        let mut i = 0;
+        while i < N {
+            arr[i] = i as u8;
+            i += 1;
+        }
+
+        arr
+    }
+
+    const DATA: [u8; 1000] = make_data::<1000>();
+
+    #[test]
+    fn across_slices() {
+        let slices = [
+            &DATA[0..100],
+            &DATA[200..300],
+            &DATA[400..500],
+            &DATA[600..700],
+        ];
+
+        let virtual_res = VirtualResource::from_slices(&slices);
+
+        let bytes = virtual_res.get_bytes(180, 200).unwrap();
+
+        assert_eq!(bytes[0..20], DATA[280..300]);
+        assert_eq!(bytes[20..120], DATA[400..500]);
+        assert_eq!(bytes[120..200], DATA[600..680]);
     }
 }
